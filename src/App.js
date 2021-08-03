3978afcbeed0c844fcc1143a37f128e63aaae1c5
Leon Erasmus <leon@santiagobiltong.cl>
2021-08-03 13:21:11 -0400

    project complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ export default function App() {
   const [inputValue, setInputValue] = useState("");
   const [entradas, setEntradas] = useState([]);
 
+
+
   useEffect(() => {
     console.log(
       "mi arreglo tiene",
@@ -16,7 +18,7 @@ export default function App() {
   }, [entradas]);
 
   const validateInput = () => {
-    // === COMPARACIÓN ESTRICTA
+    // === STRICT COMPARISON
     if (inputValue === "") {
       console.log("The input cannot be empty");
     } else {
@@ -24,6 +26,8 @@ export default function App() {
       setEntradas([...entradas, inputValue]);
     }
   };
+
+  // === FUNCTION TO HANDLE LIST REMOVAL
 
   function handleremove(index) {
     console.log(index);
@@ -35,37 +39,44 @@ export default function App() {
 
   return (
     <div>
-      <div className="container mt-5">
-        <h1 className="text-center mb-4">To-Do List</h1>
-        <div className="input-group mb-3">
+      <div className="container-fluid mt-5">
+        <div className="row justify-content-center">
+          <div className="col-8">
+            <h1 className="text-center text-secondary mb-4">To-do List</h1>
+            <div className="input-group mb-3">
 
-          <input
-          className="form-control"
-          placeholder="Add something to your list..."
-            type="text"
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-          />
+              <input
+                className="form-control"
+                placeholder="Add something to your list..."
+                type="text"
+                onChange={(e) => setInputValue(e.target.value)}
+                value={inputValue}
 
-          <button
-          className="tn btn-info input-group-text" id="basic-addon2"
-            onClick={() => {
-              validateInput();
-            }}
-          >
-            Add item
-          </button>
+              />
+
+              <button
+                className="tn btn-info text-white input-group-text" id="basic-addon2"
+                onClick={() => {
+                  validateInput();
+                  setInputValue("");
+
+                }}
+              >
+
+                <i class="fas fa-plus"></i>
+              </button>
+            </div>
+
+            <ul className="list-group">
+              {entradas.map((index, key) => (
+                <li className="list-group-item" key={key}>{index} {" "}
+
+                  <span className="float-end"><button className="btn btn-warning btn-sm" onClick={() => handleremove(index)}><i class="fas fa-trash-alt text-white m-0"></i></button></span></li>
+              ))}
+            </ul>
+            <h6 className="mt-3 text-secondary">List items <span class="badge bg-info">{entradas.length}</span></h6>
+          </div>
         </div>
-
-        <ul className="list-group">
-          {entradas.map((index, key) => (
-            <li className="list-group-item" key={key}>{index} {" "}
-
-              <span className="float-end"><button className="btn btn-warning btn-sm"   onClick={() => handleremove(index)}>Delete</button></span></li>
-          ))}
-        </ul>
-
-
       </div>
     </div>
   );
